c44db7902f106715f4c81df1e1753dac09268632
Vinod Kumar Nair <vinod827@gmail.com>
2018-12-03 21:45:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      isLoaded:false
+    }
+  }
+
+  componentDidMount(){
+     fetch('https://my-resource-server.cfapps.io/resource/uploadedfiles')
+     .then(response => {
+       response.json();
+       alert(response);
+     })
+     .then(json => {
+         this.setState({
+            isLoaded:true,
+            items:json
+         })
+     })
+  }
+
+
   render() {
+
+   var {isLoaded, items } = this.state;
+   if(!isLoaded){
+     return <div>Loading....</div>
+
+   } else{
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <ul>
+             {
+               this.items.map(item => (
+                 <li key={item.id}>
+                  //File name:{item.name} | Created date:{item.date} | Hash:{name.link}
+                  File name:{item}
+                 </li>
+               ))
+             };
+         </ul>
       </div>
     );
+  }
   }
 }
 
