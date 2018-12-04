b67e03373c077585251bc1fcb047af962ba41a09
I'm a Superman!!! <24762720+vinod827@users.noreply.github.com>
2018-12-04 17:18:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,17 +11,31 @@ class App extends Component {
   }
 
   componentDidMount(){
-     fetch('https://my-resource-server.cfapps.io/resource/uploadedfiles', {
-      headers: {
-          authorization: 'Bearer effb6f5c-574e-4419-8790-5193233f9adb'
-      }})
-     .then(response => response.json())
-     .then(json => {
-         this.setState({
-            isLoaded:true,
-            items:json,
-         })
-     });
+
+    fetch('https://my-resource-server.cfapps.io/resource/uploadedfiles', {
+              mode: 'no-cors',
+              method: "GET",
+              headers: {
+                  'Accept': 'application/json',
+                            'Content-Type': ' application/json',
+                            'X-API-SERVER': '85499f9f',
+                  'Authorization': 'Bearer 74b1976b-d814-41cd-90ec-8d5059e85217'
+                        },
+            }).then(res => res.json())
+              .then(res => {
+                if (res.status === 200){
+                  console.log("accepted");
+                  this.setState({
+                     isLoaded:true,
+                     items:res,
+                  })
+                }else {
+                  console.log(res.error);
+                }
+
+                 console.log(res.error)
+              }).catch(err => console.log(err))
+
   }
 
 
